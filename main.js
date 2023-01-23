61696ce2be5113606f4fa0af9820f17375e62ca5
Devkuroi <joan2000oviedo@gmail.com>
2023-01-22 22:58:32 -0500

    commit 11 "final"

--- main.js
+++
@@ -1,8 +0,0 @@
-var button = document.querySelector('.btn-whatsapp');
-
-button.addEventListener("transitionend", function (event) {
-	if (event.propertyName === "width" || event.propertyName === "height") {
-		button.querySelector('.logo-whatsapp').style.display = "none";
-		button.querySelector('.text').style.display = "block";
-	}
-});

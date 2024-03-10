7193b3d8b3e8bff0bf37b5149d8efefe887f7574
kuroi <joan2000oviedo@gmail.com>
2024-03-10 12:07:31 -0500

    commit 2

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,12 +1,17 @@
 document.querySelector("#container-bars").addEventListener("click", animateBars)
 
+
 let line1 = document.querySelector("#line1")
 let line2 = document.querySelector("#line2")
 let line3 = document.querySelector("#line3")
+let options = document.querySelector("#container-options")
 
 function animateBars() {
     line1.classList.toggle("activeLine1")
     line2.classList.toggle("activeLine2")
     line3.classList.toggle("activeLine3")
 
+    //options.classList.toggle("opacity-0")
+    options.classList.toggle("-translate-x-full");
+    //options.classList-toggle("hidden")
 }
